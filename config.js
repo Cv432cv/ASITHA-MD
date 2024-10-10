84574e15b3228d3b32a7c0135c3ca818e734bc92
Cv432cv <vidarshanarajapaksha39@gmail.com>
2024-10-10 18:37:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bctlTYgQ#4VUmmW_tjuh-96msZ7s36MC-CqOK7DNGahD64oBcxNw",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WyQD2JYC#Ej7JxUjtnR5bbSgNYMm5KMkXAVrmRXxkh3pHmJy0cxE",
 MONGODB: process.env.MONGODB || "mongodb+srv://nimafree:nimafree@nimafree.ae8e2.mongodb.net/",//enter mongo db url
 };
